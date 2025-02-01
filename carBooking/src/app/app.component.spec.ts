33b13ea6839f577cace9a98a79b853f2a6b3b88e
VishhKumar <vishrant22kumar@gmail.com>
2025-02-01 10:22:57 +0530

    setup

--- carBooking/src/app/app.component.spec.ts
+++ carBooking/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'carBooking' title`, () => {
+  it(`should have the 'carRental' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('carBooking');
+    expect(app.title).toEqual('carRental');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, carBooking');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, carRental');
   });
 });
--- carBooking/src/app/app.component.ts
+++ carBooking/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LandingPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'carBooking';
+  title = 'carRental';
 }
